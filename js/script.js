45407c07bd86dd2b786958902a15aea8ed836e4f
YanaBrewster <yana.brewster@gmail.com>
2020-02-12 15:06:44 +1300

    data added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
-// console.log("js");
-
-
-// // JSON FROM EXTERNAL FILE
-
-// var person = JSON.parse(myData);
-// console.log(person);
-
- $(document).ready(function(){
+$(document).ready(function(){
 
 // AJAX METHOD
 
 $.ajax({
-  url:'js/data.json',
+  url:'js/fav_data.json',
   type:'GET',
   data:'json',
   success: function(data){
@@ -19,11 +11,12 @@ $.ajax({
     var i;
     for (i=0; i<data.length; i++){
       document.getElementById('result').innerHTML +=
-      '<div class="col col-sm-6 col-md-4 col-lg-4 border border-secondary rounded mx-1 my-1">' +
-      '<h1>' + data[i].first_name  + ' ' +
-       data[i].last_name + '</h1>' +
-       '<h2>' + data[i].gender + '</h2>' +
-       '<h3>' + data[i].email + '<h3>' +
+      '<div class="col col-sm-6 col-md-4 col-lg-4 border border-secondary rounded mx-1 my-1 px-4 mx-4">' +
+      '<h1> Name: ' + data[i].first_name + '</h1>'+
+      '<h3> Gender: ' + data[i].gender + '</h3>' +
+      '<h3> Animal: ' + data[i].animal + '</h3>' +
+       '<h3> Holiday: ' + data[i].holiday + '<h3>' +
+       '<h3> Color: ' + data[i].color + '<h3>' +
       '</div>';
     }
 
